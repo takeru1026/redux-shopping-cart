30f1725b5e43ac50253c416378d392a3e0466197
takeru1026 <68760801+takeru1026@users.noreply.github.com>
2023-09-20 08:20:44 +0900

    :tada:create container

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import CartItem from './CartItem';
 
 const CartContainer = () => {
-	const { amount } = useSelector((state) => state.cart);
+	const { amount, cartItems, total } = useSelector((state) => state.cart);
 	if (amount < 1) {
 		return (
-			<div>
-				<section className="cart">
-					<header>
-						<h2>買い物かご</h2>
-						<h4 className="empty-cart">何も入っていません</h4>
-					</header>
-				</section>
-			</div>
+			<section className="cart">
+				<header>
+					<h2>買い物かご</h2>
+					<h4 className="empty-cart">何も入っていません</h4>
+				</header>
+			</section>
 		);
 	}
+	return (
+		<section className="cart">
+			<header>
+				<h2>買い物かご</h2>
+			</header>
+			<div>
+				{cartItems.map((item) => {
+					return <CartItem key={item.id} />;
+				})}
+			</div>
+			<footer>
+				<hr />
+				<div className="cart-total">
+					<h4>
+						合計 <span>{total}円</span>
+					</h4>
+				</div>
+				<button className="btn clear-btn">全削除</button>
+			</footer>
+		</section>
+	);
 };
 
 export default CartContainer;
