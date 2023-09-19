0673a0cf547c74a12161573a9b68e5501ba161fd
takeru1026 <68760801+takeru1026@users.noreply.github.com>
2023-09-20 08:27:57 +0900

    :tada:get useSelector

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -21,7 +21,7 @@ const CartContainer = () => {
 			</header>
 			<div>
 				{cartItems.map((item) => {
-					return <CartItem key={item.id} />;
+					return <CartItem key={item.id} {...item} />;
 				})}
 			</div>
 			<footer>
