2170d71496d67aea0fb85b9bb857867befaca7eb
takeru1026 <68760801+takeru1026@users.noreply.github.com>
2023-09-22 08:08:38 +0900

    :tada:delete

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import CartItem from './CartItem';
+import { clearCart } from '../features/cart/CartSlice';
 
 const CartContainer = () => {
 	const { amount, cartItems, total } = useSelector((state) => state.cart);
+	const dispatch = useDispatch();
 	if (amount < 1) {
 		return (
 			<section className="cart">
@@ -31,7 +33,9 @@ const CartContainer = () => {
 						合計 <span>{total}円</span>
 					</h4>
 				</div>
-				<button className="btn clear-btn">全削除</button>
+				<button className="btn clear-btn" onClick={() => dispatch(clearCart())}>
+					全削除
+				</button>
 			</footer>
 		</section>
 	);
--- src/features/cart/CartSlice.js
+++ src/features/cart/CartSlice.js
@@ -4,8 +4,8 @@ import cartItems from '../../cartItems';
 // 買い物カゴの初期化
 const initialState = {
 	cartItems: cartItems,
-	amount: 4,
-	total: 0,
+	// amount: 4,
+	// total: 0,
 };
 
 const cartSlice = createSlice({
@@ -13,7 +13,8 @@ const cartSlice = createSlice({
 	initialState,
 	reducers: {
 		clearCart: (state) => {
-			state.cartItems = [];
+			// state.cartItems = [];
+			return { cartItems: [], amount: 0, total: 0 };
 		},
 	},
 });
