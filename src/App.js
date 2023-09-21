97246bbb6f6ccbc26ea702aa56cab9113b78e6ff
takeru1026 <68760801+takeru1026@users.noreply.github.com>
2023-09-22 08:33:59 +0900

    :tada:useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { useEffect } from 'react';
 import './App.css';
 import CartContainer from './components/CartContainer';
 import Navbar from './components/Navbar';
+import { useDispatch, useSelector } from 'react-redux';
+import { calculateTotal } from './features/cart/CartSlice';
 
 function App() {
+	const { cartItems } = useSelector((state) => state.cart);
+	const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(calculateTotal());
+	}, [cartItems]);
 	return (
 		<main>
 			<Navbar />
