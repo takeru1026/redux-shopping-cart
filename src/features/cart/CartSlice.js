1ec0cd63b8569b7039258f19fa6a14a6a570cce3
takeru1026 <68760801+takeru1026@users.noreply.github.com>
2023-09-22 08:13:06 +0900

    :tada:create reducer

--- src/features/cart/CartSlice.js
+++ src/features/cart/CartSlice.js
@@ -4,8 +4,8 @@ import cartItems from '../../cartItems';
 // 買い物カゴの初期化
 const initialState = {
 	cartItems: cartItems,
-	// amount: 4,
-	// total: 0,
+	amount: 4,
+	total: 0,
 };
 
 const cartSlice = createSlice({
@@ -16,8 +16,9 @@ const cartSlice = createSlice({
 			// state.cartItems = [];
 			return { cartItems: [], amount: 0, total: 0 };
 		},
+		removeItem: (state, action) => {},
 	},
 });
 
-export const { clearCart } = cartSlice.actions;
+export const { clearCart, removeItem } = cartSlice.actions;
 export default cartSlice.reducer;
